71aab7cda94897549062f499b75c7161aa8d2d7e
DekinsVic <victorakinyemi42@gmail.com>
2024-11-27 15:34:59 +0100

    Update config.ts
    A Creative Visionary, A Digital Maverick in the Digital Marketing and Graphic Design World

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Victor Akinyemi | My Personal Portfolio Website';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_DESCRIPTION = 'A Creative Visionary, A Digital Maverick in the Digital Marketing and Graphic Design World.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
